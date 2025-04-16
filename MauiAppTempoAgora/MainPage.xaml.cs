90fb00097bf5e74828a1a198184787c44d419207
Pedro Henrique <89263347+Pedro-HAS32@users.noreply.github.com>
2025-04-15 21:11:42 -0300

    Configuração inicial da localização.

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -8,14 +8,14 @@ namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        //int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private async void Button_Clicked_1(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -52,6 +52,53 @@ namespace MauiAppTempoAgora
             catch (Exception ex)
             {
                 await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                //Faz a requisição da Geolocation.
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if(local != null)
+                {
+                    string local_dispositivo = $"Latitude: {local.Latitude}     \n" +
+                                               $"Longitude: {local.Longitude}   \n";
+
+                    lbl_coords.Text = local_dispositivo;
+                }
+                else
+                {
+                    lbl_coords.Text = "Nenhuma Localização";
+                }
+            }
+
+            //Mosta uma mensagem de erro caso o dispositivo não suporte a feature.
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Dispositivo não suporta", fnsEx.Message, "OK");
+            }
+
+            //Mosta uma mensagem de erro caso a feature não esteja atividad.
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Feature não está habilitada", fneEx.Message, "OK");
+            }
+
+            //Mostra uma mensagem de erro caso a permissão da localização não esteja habilitada.
+            catch (PermissionException perEx)
+            {
+                await DisplayAlert("Erro: Sem permissão de localização", perEx.Message, "OK");
+            }
+
+            //Mostra uma mensagem para erros genéricos.
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: ", ex.Message, "OK");
             }
         }
     }
